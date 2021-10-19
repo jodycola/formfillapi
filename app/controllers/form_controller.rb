75b95415af65f0ad4f5e0cd36f2ad624d9e48c0d
Kody Samaroo <kody.samaroo@gmail.com>
2021-10-19 13:32:11 -0400

    added routes, models and controllers

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,7 +1,13 @@
 class FormController < ApplicationController
 
+    def forms
+        forms = Forms.all
+        render json: forms
+    end
+
     def submit
         form = Form.create(first: params[:first], last: params[:last], email: params[:email], company: params[:company])
+        render json: form
     end
 
 end
